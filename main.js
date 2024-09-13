11ac91fbdb47e267c73545999f15b39f4cb8fa05
Ana Sena <luiza1fofa@gmail.com>
2024-09-13 11:27:50 -0300

    atualizando com fetch

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $(botao).find('i').addClass('d-none');   //Adicionando o botão de carregamento depois da busca
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function(resposta){  //Adicionando o conteudo encontrado à barra de Endereço
+        /*$.ajax(endpoint).done(function(resposta){  //Adicionando o conteudo encontrado à barra de Endereço
             const logradouro = resposta.logradouro;
             const bairro = resposta.bairro;
             const cidade = resposta.localidade;
@@ -21,8 +21,37 @@ $(document).ready(function(){
             setTimeout(function(){     //Removendo o botão de carregamento depois da busca
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
-            },500)
+            },500)*/
             
+            fetch(endpoint).then(function(resposta){
+                return resposta.json();
+            })
+            .then(function(json){
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
+                $('#endereco').val(endereco);
+            })
+                    
+            .catch(function(erro){ //Se houver algum erro, roda o catch
+                alert("Ocorreu um erro ao buscar o endereço")
+            })
+
+            .finally(function(){ //Roda o finally independente de erro
+                setTimeout(function(){     
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                },500)
+            })
         })
-    })
+
+        $('#formulario-pedido').submit(function(evento){
+            evento.preventDefault();
+
+            if($('#nome').val().length == 0){
+                throw new Error ('Digite o nome!');
+            }
+        })
 })
